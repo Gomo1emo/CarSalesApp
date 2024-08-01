ef465b861af854749d27455d85a07d99d1e9207a
Gomolemo <tshabalalagomolemo@gmail.com>
2024-08-01 07:27:39 +0200

    tobirama

--- CarSales-App/src/app/Pages/showcase/showcase.component.ts
+++ CarSales-App/src/app/Pages/showcase/showcase.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductsService } from 'src/app/services/products/products.service';
+
 
 @Component({
   selector: 'app-showcase',
@@ -7,4 +10,19 @@ import { Component } from '@angular/core';
 })
 export class ShowcaseComponent {
 
+  car: any[] = []
+
+  constructor(private router: Router, private product: ProductsService) { }
+
+
+  ngOnInit(): void {
+    this.getProductById(1); // Replace 1 with the desired product ID
+  }
+
+  getProductById(id: number): void {
+    this.product.getProduct(id).subscribe((response) => {
+      this.car = response;
+      console.log(this.car);
+    });
+  }
 }
