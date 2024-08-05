f854209ab7851a367385af7292890ea01166954d
Gomolemo <tshabalalagomolemo@gmail.com>
2024-08-05 07:13:27 +0200

    G

--- CarSales-App/src/app/Pages/sign-up/sign-up.component.ts
+++ CarSales-App/src/app/Pages/sign-up/sign-up.component.ts
@@ -7,4 +7,74 @@ import { Component } from '@angular/core';
 })
 export class SignUpComponent {
 
+  regForm!: FormGroup;
+  submitted = false;
+
+  constructor(private fb: FormBuilder, private authService: AuthServiceService) {}
+
+  ngOnInit(): void {
+    // this.regForm = this.fb.group({
+    //   name: ['', [Validators.required]],
+    //   lname: ['', [Validators.required]],
+    //   email: ['', [Validators.required, Validators.email]],
+    //   password: ['', [Validators.required, Validators.minLength(8)]],
+    //   confirmPassword: ['', [Validators.required]],
+    //   agreeTerms: [false, [Validators.requiredTrue]]
+    // }, {
+    //   validators: this.passwordMatchValidation
+    // });
+    this.regForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      lname: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      agreeTerms: new FormControl('')
+    }),{
+      validators: this.passwordMatchValidation
+    }
+
+  }
+
+  passwordMatchValidation(form: FormGroup) {
+    const password = form.get('password');
+    const confirmPassword = form.get('confirmPassword');
+    if (password?.value !== confirmPassword?.value) {
+      confirmPassword?.setErrors({ passwordMismatch: true });
+    } else {
+      confirmPassword?.setErrors(null);
+    }
+  }
+
+  get PasswordMismatch() {
+    return this.regForm.get('confirmPassword')?.errors?.['passwordMismatch'];
+  }
+
+  submit() {
+    this.submitted = true;
+    console.log("called")
+    // console.log(this.regForm.value)
+
+    if (this.regForm.valid) {
+      const registrationData = {
+        firstname: this.regForm.value.name,
+        lastname: this.regForm.value.lname,
+        email: this.regForm.value.email,
+        password: this.regForm.value.password,
+        role: "HUNTER"
+      };
+
+      this.authService.register(registrationData).subscribe(res => {
+        if (res.token !== null) {
+
+          setTimeout(() => {
+            window.location.href = '/login';
+          }, 2000)
+        } else {
+          // Handle registration error
+        }
+      });
+      console.log('Data to be sent', registrationData);
+    }
+  }
 }
